86eb70ed8c1b0db9083c4cbe52b2222fda21c969
potapenkom <it.potapenkom@gmail.com>
2021-09-01 11:41:03 +0300

    added entity quize and questions

---
+++ quizal.models/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace quizal.models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public string QuestionName { get; set; }
+
+        public string FirstOption { get; set; }
+
+        public string SecondOption { get; set; }
+
+        public string ThirdOption { get; set; }
+
+        public string FourthOption { get; set; }
+
+        public string CorrectAnswer { get; set; }
+
+        public int CorrectAnswerPoints { get; set; }
+
+        public int QuizId { get; set; }
+
+        public Quiz Quiz { get; set; }
+    }
+}
---
+++ quizal.models/Quiz.cs
@@ -0,0 +1,22 @@
+﻿using quizal.models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quizal.models
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Category Category { get; set; }
+
+        public List<Question> QuizQuestions { get; set; } = new List<Question>();
+
+        public string QuizLogoUrl { get; set; }
+    }
+}
